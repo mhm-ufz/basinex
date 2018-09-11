05fd2ef5864a6cbc77ad3ffa71bbf2653f3abf67
David Schäfer <david.schaefer@ufz.de>
2018-09-11 13:17:26 +0200

    use the correct numpy headers

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from distutils.core import setup, Extension
+
 from Cython.Build import cythonize
+import numpy as np
 
 # usage: python setup.py build_ext --inplace
 
-setup(
-    ext_modules = cythonize("extractor/extractor.pyx",
-                            language="c++",))
+extension = Extension(
+    "extractor",
+    sources=["extractor/extractor.pyx"],
+    language="c++",
+    include_dirs=[np.get_include()])
+
+setup(ext_modules=cythonize(extension))
 
