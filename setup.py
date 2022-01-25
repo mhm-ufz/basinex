c21186285f91e27ba6ff883859bf978d59a70003
Sebastian Müller <mueller.seb@posteo.de>
2022-01-25 12:16:13 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from distutils.core import setup, Extension
-
+"""Basin Extractor."""
+import os
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
-# usage: python setup.py build_ext --inplace
+# cython extensions ###########################################################
 
-extension = Extension(
-    "extractor",
-    sources=["extractor/extractor.pyx"],
-    language="c++",
-    include_dirs=[np.get_include()])
+CY_MODULES = []
+CY_MODULES.append(
+    Extension(
+        "basinex.extractor",
+        [os.path.join("src", "basinex", "extractor.pyx")],
+        language="c++",
+        include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    )
+)
+EXT_MODULES = cythonize(CY_MODULES)  # annotate=True
 
-setup(ext_modules=cythonize(extension))
+# embed signatures for sphinx
+for ext_m in EXT_MODULES:
+    ext_m.cython_directives = {"embedsignature": True}
 
+# setup #######################################################################
+
+setup(ext_modules=EXT_MODULES, include_dirs=[np.get_include()])
